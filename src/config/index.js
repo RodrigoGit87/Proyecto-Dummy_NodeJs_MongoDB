5e420ad1b027745af5a03fce04f2796607b5a8fd
RodrigoGit87 <rodrigollamas87@gmail.com>
2026-06-17 16:39:17 +0100

    port opcional

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 // Esto permite que importemos el archivo de configuracion en otras partes de la aplicacion,
 //  y acceder a las variables de entorno a través de config.port
 module.exports.config = {
-  port: process.env.PORT,
+  port: process.env.PORT || 3000,
   mongoUri: process.env.MONGO_URI,
   mongoDbname: process.env.MONGO_DBNAME,
 };
